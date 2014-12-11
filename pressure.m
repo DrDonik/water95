41640c1544aa139b6a900ce5ab13161a170d8246
Dominik Marti <dom@inik.ch>
2014-12-11 15:17:38 +0100

    Removed the cutoff from pressure at temperatures below 0C, since there can also be a liquid pressure in the metastable liquid

--- pressure.m
+++ pressure.m
@@ -1,6 +1,5 @@
 function [p, x] = pressure(rho, T)
 % returns pressure and dryness fraction as functions of rho and T
-% simply returns 0 for T < 0°C !!
 % parameters:
 %   rho      density
 %   T        temperature
@@ -11,14 +10,8 @@ function [p, x] = pressure(rho, T)
 [flag, x, rhop, rhopp, pS] = findRegion(rho, T);
 
 switch flag
-  case {1, 3, 4}          % supercritical, water, steam
+  case {1, 2, 3, 4}          % supercritical, below zero, water, steam
     p = pressureRaw(rho, T);
-  case 2                  % below Tt
-    if T >= celsiusToKelvin(0.0)
-      p = pressureRaw(rho, T);
-    else  
-      p = 0.0;      % simple cutoff
-    end
   case 5                  % on saturation curve
     p = pS;
 end
