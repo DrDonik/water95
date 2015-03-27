81f43147f8f688c5fa59e81b1bba4117cc323609
Chen Qiu <chen.qiu@iap.unibe.ch>
2015-03-27 17:55:37 +0100

    Exposed three private functions

---
+++ directAuxSaturationPressure.m
@@ -0,0 +1,8 @@
+function pS = directAuxSaturationPressure(T)
+    % exposes the helper function to calculate approximate values for the
+    % saturation pressure
+
+    pS = auxSaturationPressure(T);
+
+    return
+end
--- directPressureRaw.m
+++ directPressureRaw.m
@@ -1,4 +1,7 @@
 function p = directPressureRaw(rho, T)
+    % exposes the helper function to calculate the raw pressure from
+    % iapws95
+
     p = pressureRaw(rho, T);
     
     return
