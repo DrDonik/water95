2c74ffbe8f6829aede87daea490fa73c88ac3c27
Dominik Marti <dom@inik.ch>
2015-08-25 10:27:16 +0200

    I included the fit to calculate the saturation densities below -36 centigrades directly into the water95 routine, so that it is accessible for all the inclusion programs.

--- directAuxSaturationDensities.m
+++ directAuxSaturationDensities.m
@@ -1,8 +1,15 @@
 function [rhop, rhopp] = directAuxSaturationDensities(T)
     % exposes the helper function to calculate approximate values for the
-    % saturation pressure
-
+    % saturation densities
+    
     [rhop, rhopp] = auxSaturationDensities(T);
+    
+    % auxSaturationDensity for the liquid phase is only valid down to -36
+    % centigrades. Below that, we rely on a fit for values calculated along
+    % an iso_Th curves.
+    if T < 273.15-36
+        rhop = 16.31*sqrt(0.2167*(T-273.15+39.56))+958.4;
+    end
 
     return
 end
