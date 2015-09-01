a8f61dc3c4180fe68bc3ed62d4676f6c813c3ee7
Dominik Marti <dom@inik.ch>
2015-09-01 14:43:21 +0200

    Took out the work around for low temperatures, because it gave a discontinuity.

--- directAuxSaturationDensities.m
+++ directAuxSaturationDensities.m
@@ -4,12 +4,5 @@ function [rhop, rhopp] = directAuxSaturationDensities(T)
     
     [rhop, rhopp] = auxSaturationDensities(T);
     
-    % auxSaturationDensity for the liquid phase is only valid down to -36
-    % centigrades. Below that, we rely on a fit for values calculated along
-    % an iso_Th curves.
-    if T < 273.15-36
-        rhop = 16.31*sqrt(0.2167*(T-273.15+39.56))+958.4;
-    end
-
     return
 end
