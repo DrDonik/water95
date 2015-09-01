c122e376a073cab34b3d01a0645a93039a5375b9
Dominik Marti <dom@inik.ch>
2015-09-01 15:51:05 +0200

    Removed the two directAuxSaturationDensities and directAuxSaturationPressure

--- directAuxSaturationDensities.m
+++
@@ -1,8 +0,0 @@
-function [rhop, rhopp] = directAuxSaturationDensities(T)
-    % exposes the helper function to calculate approximate values for the
-    % saturation densities
-    
-    [rhop, rhopp] = auxSaturationDensities(T);
-    
-    return
-end
--- directAuxSaturationPressure.m
+++
@@ -1,8 +0,0 @@
-function pS = directAuxSaturationPressure(T)
-    % exposes the helper function to calculate approximate values for the
-    % saturation pressure
-
-    pS = auxSaturationPressure(T);
-
-    return
-end
