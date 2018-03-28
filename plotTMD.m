62d270b4d6b94935d12c31f092c85d96eede5fb1
Dominik Marti <domar@fotonik.dtu.dk>
2018-03-28 17:00:25 +0200

    Trying a simple minimisation function to find the TMD.

--- plotTMD.m
+++ plotTMD.m
@@ -1,22 +1,23 @@
-function [p, rho] = plotTMD(T)
+function [T, rho] = plotTMD(p)
 %plotTMD plots the temperature of maximum density line in a p-T diagram
 % parameters:
-%   T        temperature
+%   p        pressure [Pa]
 % results:
-%   p        pressure on the TMD
-%   rho      density on the TMD
+%   T        temperature  on the TMD [K]
+%   rho      density on the TMD [kg/m^3]
 
-global IAPWS95_COEFFS;
-if isempty(IAPWS95_COEFFS)
-    IAPWS95_COEFFS = readIAPWS95data();
+TmD = @(x) -localDensity(p, x);
+
+[T, rho] = fminbnd(TmD, 2+273.15, 6+273.15);
+
+rho = -rho;
+
 end
 
-% unpack coefficients
-[R,Tc,rhoc] = IAPWS95_COEFFS{1:3};
+function rho = localDensity(p, T)
 
-% dp_dtau = @(rho,T) -pressureRaw(rho, T)/(Tc/T)*(rho/rhoc)*phir_dt((rho/rhoc), (Tc/T), coeffs);
-dp_dtau = @(rho,T) pressureRaw(rho, T)*T - R*T*rho*(1 + (rho/rhoc)*phir_dt((rho/rhoc), (Tc/T), coeffs)*T^2/Tc);
+func = @(x) pressureRaw(x,T) - p; 
+rho = fzero(func, 999);
 
-[rho, p] = fzero(dp_dtau, [0, 0]);
-
+return
 end
